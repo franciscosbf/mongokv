aebff0327e0b7eeb6507c2f63d325fe8f085756a
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-10-31 12:22:14 +0000

    fixes, upsert refactoring and implement get_int8

--- mongokv.c
+++ mongokv.c
@@ -5,7 +5,6 @@
 #include "utils/elog.h"
 #include "utils/hsearch.h"
 #include "utils/builtins.h"
-#include "utils/palloc.h"
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
@@ -13,7 +12,7 @@
 PG_MODULE_MAGIC;
 
 #define MAX_COLLECTION_NAME 12
-#define MAX_COLLECTIONS 10
+#define INITIAL_COLLECTIONS_CACHE_SZ 10
 
 typedef char collection_key[MAX_COLLECTION_NAME + 1 /* '\0' */];
 typedef struct {
@@ -27,8 +26,7 @@ void cleanup(void);
 void check_client(void);
 void to_collection_entry(const char *collection_name, collection_key *key);
 void create_collection(const collection_key *key,
-                       collection_entry *entry,
-                       const char *collection_name);
+                       collection_entry *entry);
 mongoc_collection_t *fetch_collection(const char* collection_name);
 
 static mongoc_client_t *client = NULL;
@@ -49,7 +47,7 @@ void init_collections_cache(void) {
     .entrysize = sizeof(collection_entry),
   };
 
-  collections_cache = hash_create("collections_cache", MAX_COLLECTIONS,
+  collections_cache = hash_create("collections_cache", INITIAL_COLLECTIONS_CACHE_SZ,
                                   &info, HASH_ELEM & HASH_BLOBS);
 }
 
@@ -92,8 +90,7 @@ void to_collection_entry(const char *collection_name, collection_key *key) {
 }
 
 void create_collection(const collection_key *key,
-                       collection_entry *entry,
-                       const char *collection_name) {
+                       collection_entry *entry) {
   const char *database_name = NULL;
   bson_t *index_key = NULL;
   mongoc_index_model_t *index_model = NULL;
@@ -128,10 +125,82 @@ mongoc_collection_t *fetch_collection(const char* collection_name) {
 
   entry = hash_search(collections_cache, &key, HASH_ENTER, &found);
   if (!found)
-    create_collection(&key, entry, collection_name);
+    create_collection(&key, entry);
 
   return entry->collection;
 }
+
+#define UPSERT(COLLECTION_NAME_TEXT, KEY_TEXT, VALUE_BCON) \
+  char* collection_name_cstring = NULL; \
+  mongoc_collection_t *collection = NULL; \
+  char* key_cstring = NULL; \
+  bson_t *selector = NULL; \
+  bson_t *update = NULL; \
+  bson_t *opts = NULL; \
+  bool success; \
+  bson_error_t error; \
+  \
+  collection_name_cstring = text_to_cstring(COLLECTION_NAME_TEXT); \
+  collection = fetch_collection(collection_name_cstring); \
+  \
+  key_cstring = text_to_cstring(KEY_TEXT); \
+  \
+  selector = BCON_NEW("key", BCON_UTF8(key_cstring)); \
+  update = BCON_NEW("$set", "{", "key", BCON_UTF8(key_cstring), "value", VALUE_BCON, "}"); \
+  opts = BCON_NEW("upsert", BCON_BOOL(true)); \
+  success = mongoc_collection_update_one(collection, selector, update, opts, NULL, &error); \
+  bson_destroy(selector); \
+  bson_destroy(update); \
+  bson_destroy(opts); \
+  if (!success) { \
+    elog(ERROR, "failed to put value: %s", error.message); \
+  }
+
+#define GET(COLLECTION_NAME_TEXT, \
+            KEY_TEXT, \
+            BSON_ITER_HOLDS, \
+            BSON_ITER_PARSE, \
+            HOLDER) \
+  char* collection_name_cstring = NULL; \
+  mongoc_collection_t *collection = NULL; \
+  char* key_cstring = NULL; \
+  bson_t *filter = NULL; \
+  bool found; \
+  bool with_error; \
+  mongoc_cursor_t *cursor; \
+  bson_t *doc; \
+  bson_error_t error; \
+  bson_iter_t iter; \
+  const char *error_message = NULL; \
+  \
+  collection_name_cstring = text_to_cstring(COLLECTION_NAME_TEXT); \
+  collection = fetch_collection(collection_name_cstring); \
+  \
+  key_cstring = text_to_cstring(KEY_TEXT); \
+  \
+  filter = BCON_NEW("key", BCON_UTF8(key_cstring)); \
+  cursor = mongoc_collection_find_with_opts(collection, filter, NULL, NULL); \
+  bson_destroy(filter); \
+  found = mongoc_cursor_next(cursor, (const bson_t **)&doc); \
+  with_error = mongoc_cursor_error(cursor, &error); \
+  mongoc_cursor_destroy(cursor); \
+  if (!found) { \
+    if (with_error) \
+      elog(ERROR, "failed to find key: %s", error.message); \
+    else \
+      elog(ERROR, "key doesn't exist"); \
+  } \
+  \
+  if (!bson_iter_init_find(&iter, doc, "value")) { \
+    bson_destroy(doc); \
+    elog(ERROR, "value field is somehow missing in pair"); \
+  } \
+  if (!BSON_ITER_HOLDS(&iter)) { \
+    bson_destroy(doc); \
+    elog(ERROR, "key doesn't hold value of expected type"); \
+  } \
+  HOLDER = BSON_ITER_PARSE(&iter); \
+  bson_destroy(doc);
 
 void _PG_init(void) {
   mongoc_init();
@@ -159,23 +228,19 @@ Datum create_client(PG_FUNCTION_ARGS) {
     elog(ERROR, "client is already created");
 
   dns_cstring = text_to_cstring(dns);
-  uri = mongoc_uri_new_with_error(dns_cstring, &error);
-  pfree(dns_cstring);
-  if (uri == NULL) {
-    cleanup_connection();
 
+  if ((uri = mongoc_uri_new_with_error(dns_cstring, &error)) == NULL) {
+    cleanup_connection();
     elog(ERROR, "failed to parse connection uri: %s", error.message);
   }
 
   if ((database_name = mongoc_uri_get_database(uri)) == NULL) {
     cleanup_connection();
-
     elog(ERROR, "uri doesn't have database");
   }
 
   if ((client = mongoc_client_new_from_uri_with_error(uri, &error)) == NULL) {
     cleanup_connection();
-
     elog(ERROR, "failed to create client: %s", error.message);
   }
 
@@ -186,7 +251,6 @@ Datum create_client(PG_FUNCTION_ARGS) {
   bson_destroy(ping_command);
   if (!successful_ping) {
     cleanup_connection();
-
     elog(ERROR, "failed to check connection with database: %s", error.message);
   }
 
@@ -215,16 +279,8 @@ PG_FUNCTION_INFO_V1(put_int8);
 
 Datum put_int8(PG_FUNCTION_ARGS) {
   text* collection_name = NULL;
-  char* collection_name_cstring = NULL;
   text* key = NULL;
-  char* key_cstring = NULL;
   int64 value;
-  mongoc_collection_t *collection = NULL;
-  bson_t *selector = NULL;
-  bson_t *update = NULL;
-  bson_t *opts = NULL;
-  bool success;
-  bson_error_t error;
 
   check_client();
 
@@ -232,24 +288,9 @@ Datum put_int8(PG_FUNCTION_ARGS) {
   key = PG_GETARG_TEXT_PP(1);
   value = PG_GETARG_INT64(2);
 
-  collection_name_cstring = text_to_cstring(collection_name);
-  collection = fetch_collection(collection_name_cstring);
-  pfree(collection_name_cstring);
+  UPSERT(collection_name, key, BCON_INT64(value));
 
-  key_cstring = text_to_cstring(key);
-  selector = BCON_NEW("key", BCON_UTF8(key_cstring));
-  update = BCON_NEW("$set", "{", "key", BCON_UTF8(key_cstring), "value", BCON_INT64(value), "}");
-  opts = BCON_NEW("upsert", BCON_BOOL(true));
-  success = mongoc_collection_update_one(collection, selector, update, opts, NULL, &error);
-  bson_destroy(selector);
-  bson_destroy(update);
-  bson_destroy(opts);
-  pfree(key_cstring);
-  if (!success) {
-    elog(ERROR, "failed to put value: %s", error.message);
-  }
-
-  elog(INFO, "int8 put with success");
+  elog(INFO, "int8 stored with success");
 
   PG_RETURN_VOID();
 }
@@ -257,5 +298,22 @@ Datum put_int8(PG_FUNCTION_ARGS) {
 PG_FUNCTION_INFO_V1(get_int8);
 
 Datum get_int8(PG_FUNCTION_ARGS) {
-  PG_RETURN_INT64(0);
+  text* collection_name = NULL;
+  text* key = NULL;
+  int64 value;
+
+  check_client();
+
+  collection_name = PG_GETARG_TEXT_PP(0);
+  key = PG_GETARG_TEXT_PP(1);
+
+  GET(collection_name,
+      key,
+      BSON_ITER_HOLDS_INT64,
+      bson_iter_int64,
+      value);
+
+  elog(INFO, "int8 returned with success");
+
+  PG_RETURN_INT64(value);
 }
